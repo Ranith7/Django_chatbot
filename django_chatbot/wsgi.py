f0f3e9e291b855289987797d198b838f72db3dd8
Ranith7 <ranithbalakrishnan7@gmail.com>
2025-09-16 16:54:19 +0530

    Auto-run migrations on startup for Render Free

--- django_chatbot/wsgi.py
+++ django_chatbot/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chatbot.settings')
 
+# Setup Django
+import django
+django.setup()
+
+# Automatically run migrations on startup
+from django.core.management import call_command
+try:
+    call_command("migrate", interactive=False)
+except Exception as e:
+    print("Migration failed:", e)
+
+# WSGI application
 application = get_wsgi_application()
